cb588f774b8b05f5a3c362c48dbf444acfc87360
possiblyHugo <alainhandzar@gmail.com>
2024-05-14 01:47:32 -0700

    + Convert Function

--- src/HexToDec.h
+++ src/HexToDec.h
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <cmath>
 
 class HexToDec {
 public:
 	HexToDec(std::string hexString);
-	int HexDigitToDec(char digit);
+	int Convert();
 
 private:
+	std::string hexString;
 	const std::unordered_map<char, int> HexList{
 	{ 'A', 10 },
 	{ 'B', 11 },
@@ -17,10 +19,29 @@ private:
 	{ 'E', 14 },
 	{ 'F', 15 }
 	};
+
+	int HexDigitToDec(char digit);
 };
 
-HexToDec::HexToDec(std::string hexString) {
+HexToDec::HexToDec(std::string hexStringInput) {
+	hexString = hexStringInput;
+}
 
+int HexToDec::Convert() {
+	int result{ 0 };
+	int decValue{ 0 };
+
+	for (int i = hexString.length() - 1; i >= 0; i--) {
+		decValue = HexDigitToDec(hexString[i]);
+		
+		if (decValue == -1) {
+			std::cout << "There was an error converting HexDigit to Dec\n";
+			exit(-1);
+		}
+		
+		result += decValue + std::pow(16, i);
+	}
+	return result;
 }
 
 int HexToDec::HexDigitToDec(char digit) {
