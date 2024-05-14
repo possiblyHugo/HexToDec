6cc04029e1abaa7a0b6d25a47d2eed0079818494
possiblyHugo <alainhandzar@gmail.com>
2024-05-14 01:39:01 -0700

    + HexDigitToDec function

--- src/HexToDec.h
+++ src/HexToDec.h
@@ -6,9 +6,10 @@
 class HexToDec {
 public:
 	HexToDec(std::string hexString);
+	int HexDigitToDec(char digit);
 
 private:
-	std::unordered_map<char, int> HexList{
+	const std::unordered_map<char, int> HexList{
 	{ 'A', 10 },
 	{ 'B', 11 },
 	{ 'C', 12 },
@@ -16,9 +17,20 @@ private:
 	{ 'E', 14 },
 	{ 'F', 15 }
 	};
-
 };
 
 HexToDec::HexToDec(std::string hexString) {
 
+}
+
+int HexToDec::HexDigitToDec(char digit) {
+	if (isdigit(digit)) {
+		return digit - '0';
+	}
+	else if (HexList.find(digit) != HexList.end()) {
+		return HexList.at(digit);
+	}
+	else {
+		return -1;
+	}
 }
